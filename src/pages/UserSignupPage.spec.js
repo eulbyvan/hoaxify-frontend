31e9496aadb2f8406aa05d580cc43cade2172ffb
eulbyvan <work.issdq@gmail.com>
2022-12-14 20:21:22 +0700

    add input change handler

--- src/pages/UserSignupPage.spec.js
+++ src/pages/UserSignupPage.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import UserSignupPage from "./UserSignupPage";
 
@@ -73,6 +73,52 @@ describe('UserSignupPage', () => {
 
         });
 
+    });
+
+    describe('Interactions', () => {
+        const changeEvent = (content) => {
+            return {
+                target: {
+                    value: content
+                }
+            };
+        };
+
+        it('sets the display name value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />);
+            const displayNameInput = queryByPlaceholderText('Your display name');
+
+            fireEvent.change(displayNameInput, changeEvent('my-display-name'));
+
+            expect(displayNameInput).toHaveValue('my-display-name');
+        });
+
+        it('sets the username value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />);
+            const usernameInput = queryByPlaceholderText('Your username');
+
+            fireEvent.change(usernameInput, changeEvent('my-username'));
+
+            expect(usernameInput).toHaveValue('my-username');
+        });
+
+        it('sets the password value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />);
+            const passwordInput = queryByPlaceholderText('Your password');
+
+            fireEvent.change(passwordInput, changeEvent('my-password'));
+
+            expect(passwordInput).toHaveValue('my-password');
+        });
+
+        it('sets the password repeat value into state', () => {
+            const { queryByPlaceholderText } = render(<UserSignupPage />);
+            const passwordRepeat = queryByPlaceholderText('Repeat your password');
+
+            fireEvent.change(passwordRepeat, changeEvent('my-password-repeat'));
+
+            expect(passwordRepeat).toHaveValue('my-password-repeat');
+        });
     });
 
 });
